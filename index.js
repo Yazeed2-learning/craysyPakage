ca619037850ee1c7e862a8e2abd6e04d4e260180
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-10-23 12:37:40 +0300

    anniong code

---
+++ index.js
@@ -0,0 +1,13 @@
+function yazeed(){
+    
+        console.log('the world is great '); 
+        console.log('physics is great '); 
+for (let index = 0; index < 100; index++) {
+    console.log('hello');
+    
+    
+}
+
+}
+
+madule.exprots = yazeed()
